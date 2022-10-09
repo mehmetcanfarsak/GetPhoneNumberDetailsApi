ea25d76ead49aa465744029479f033302a4d9347
mehmetcanfarsak <mehmetcanfarsak@gmail.com>
2022-10-09 16:20:52 +0300

    small bug fixes and improvements

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ from phonenumbers import geocoder
 from pydantic import BaseModel
 import markdown2
 from fastapi.responses import HTMLResponse
-
+from phonenumbers import carrier
+from phonenumbers import timezone
 class PhoneNumberDetail(BaseModel):
     country: Union[str, None]
+    countryOrLocation: Union[str, None]
+    carrier: Union[str, None]
     country_code: Union[int, None]
     national_number: Union[int, None]
     country_code_source: Union[int, None]
     country_code_source: Union[int, None]
+    is_possible_number: Union[bool] = False
+    is_valid_number: Union[bool] = False
+    time_zones_for_number: Union[list, None]
+
 
 
 app = FastAPI()
@@ -22,12 +29,19 @@ def root():
         readme_content=file.read()
     return markdown2.markdown(readme_content)
 
-@app.get("/get_phone_number_details",response_model=PhoneNumberDetail,description="Checks if phone number is valid and returns country of number.")
+@app.get("/get_phone_number_details",response_model=PhoneNumberDetail,description="Checks if phone number is valid and returns country of number. Example: (+442083661177)")
 def get_phone_number_details(phone_number:str=Query(description="Phone number starting with +")):
+    if(phone_number.startswith(" ")):
+        phone_number="+"+phone_number[1:]
     try:
         phone_number_details=phonenumbers.parse(phone_number)
         phone_number_details_dict=phone_number_details.__dict__
-        phone_number_details_dict['country']=geocoder.description_for_number(phone_number_details,"en")
+        phone_number_details_dict['country']=geocoder.country_name_for_number(phone_number_details, "en")
+        phone_number_details_dict['countryOrLocation']=geocoder.description_for_number(phone_number_details,"en")
+        phone_number_details_dict['carrier']=carrier.name_for_number(phone_number_details, "en")
+        phone_number_details_dict['is_possible_number']=phonenumbers.is_possible_number(phone_number_details)
+        phone_number_details_dict['is_valid_number']=phonenumbers.is_valid_number(phone_number_details)
+        phone_number_details_dict['time_zones_for_number'] = timezone.time_zones_for_number(phone_number_details)
     except:
         return PhoneNumberDetail()
     return phone_number_details_dict
